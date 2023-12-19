3f2eae72ee8ac4a1a9d6c9043365c70f548d5d1b
Eduardo Pignatelli <edu.pignatelli@gmail.com>
2023-12-19 10:25:47 +0000

    Remove unnecessary files and align install

--- exp/__init__.py
+++ exp/__init__.py
@@ -15,16 +15,6 @@
 # Make sure that the gym registry is populated
 # with exteral environments.
 from . import (
-    agents,
-    config,
-    environment,
     experiment,
-    image,
     logging,
-    mdp,
-    memory,
-    modules,
-    preprocess,
-    spaces,
-    losses
 )
